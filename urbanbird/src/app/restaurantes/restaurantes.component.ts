982f2ff909809d0dd1ed289a984f233735beff9e
Alvaro Potel <alvaropotel@gmail.com>
2021-12-14 23:10:45 +0000

    Created Oferta page; ajusted Restaurante and Diversao Components

--- urbanbird/src/app/restaurantes/restaurantes.component.ts
+++ urbanbird/src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Oferta } from 'app/shared/oferta.model';
+import { OfertasService } from 'app/ofertas.service';
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  styleUrls: ['./restaurantes.component.css'],
+  providers:[OfertasService]
 })
 export class RestaurantesComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[];
+
+  constructor(private ofertasServices: OfertasService) { }
 
   ngOnInit() {
+    this.ofertasServices.getOfertasPorCategoria('restaurante')
+    .then((ofertas:Oferta[]) => {
+      this.ofertas = ofertas
+    })
   }
+
+  //getOfertasPorCategoria
 
 }
