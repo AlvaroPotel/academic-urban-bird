072a5eea5d3ac3a278c8abff1bb4345a2780fb54
Alvaro Potel <alvaropotel@gmail.com>
2021-12-15 23:20:51 +0000

    Ajusted OfertaComponent and add getOfertaProId method

--- urbanbird/src/app/oferta/oferta.component.ts
+++ urbanbird/src/app/oferta/oferta.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { OfertasService } from 'app/ofertas.service';
+import { Oferta } from 'app/shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers:[OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public oferta: Oferta;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private ofertaService: OfertasService
+  ) { }
 
   ngOnInit() {
-    console.log('Test= ' + this.route.snapshot.params['id']);
-    
-  
+   this.ofertaService.getOfertaPorId(this.route.snapshot.params['id'])
+   .then((oferta: Oferta) =>{
+    this.oferta = oferta;
+   })
   }
 
 }
