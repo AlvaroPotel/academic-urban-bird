38cd55a1f3c0f0b87753dbf43ff88d24f1585a71
Alvaro Potel <alvaropotel@gmail.com>
2022-01-17 23:55:58 +0000

    Working with Observable

--- urbanbird/src/app/oferta/oferta.component.ts
+++ urbanbird/src/app/oferta/oferta.component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from 'app/ofertas.service';
 import { Oferta } from 'app/shared/oferta.model';
+
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers:[OfertasService]
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
   public oferta: Oferta;
 
   constructor(
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private ofertaService: OfertasService
   ) { }
 
   ngOnInit() {
-   this.ofertaService.getOfertaPorId(this.route.snapshot.params['id'])
-   .then((oferta: Oferta) =>{
-    this.oferta = oferta;
-   })
+    this.ofertaService.getOfertaPorId(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta;
+      })
+
+
   }
 
 }
--- urbanbird/src/app/topo/topo.component.ts
+++ urbanbird/src/app/topo/topo.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from 'app/ofertas.service';
+import { Oferta } from 'app/shared/oferta.model';
+import { Observable, Subject } from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
+  private subjectPesquisa: Subject<string> = new Subject<string>()
+
+  constructor(private ofertaServices: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+      .debounceTime(1000)
+      .distinctUntilChanged()
+      .switchMap((termo: string) => {
+        if (termo.trim() === '') {
+          return Observable.of<Oferta[]>([])
+        }
+        return this.ofertaServices.pesquisaOfertas(termo)
+      })
+      .catch((err: any) => {
+        console.log(err);
+        return Observable.of<Oferta[]>([])
+      })
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      console.log(ofertas);
+
+      this.ofertas2 = ofertas
+    })
   }
 
+  public pesquisa(termoDaPesquisa: string): void {
+    this.subjectPesquisa.next(termoDaPesquisa)
+  }
 }
